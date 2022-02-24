68aa35f88396a08b2cd17533990cc79fa34f438f
Hwan <65848753+ssh-24@users.noreply.github.com>
2022-02-25 01:31:11 +0900

    220225 01:30 progressBar 구현

--- practice/script.js
+++ practice/script.js
@@ -1,7 +1,33 @@
 ;(function () {
   'use strict'
 
+  let timerId
+
   const get = (target) => {
     return document.querySelector(target)
   }
+
+  // scroll 이벤트 시 성능향상을 위한 throttle
+  const throttle = (callback, time) => {
+    if (timerId) return
+    setTimeout(() => {
+      callback()
+      timerId = undefined; // 매번 초기화를 시켜줄 필요
+    }, time)
+  }
+
+  const $progressBar = get('.progress-bar')
+
+
+  const onScroll = () => {
+    const height = document.documentElement.scrollHeight -
+     document.documentElement.clientHeight
+
+    const scrollTop = document.documentElement.scrollTop
+
+    const width = (scrollTop / height) * 100;
+    $progressBar.style.width = width + '%';
+  }
+
+  window.addEventListener('scroll', () => throttle(onScroll,100));
 })()
